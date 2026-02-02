579a33e6d1bc6eedce2bd9af746e7b4f49518103
Jaymark <JaymarkAnit@gmail.com>
2026-02-02 18:49:52 +0800

    fixing

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Add click event listener to the wax seal
 document.addEventListener("DOMContentLoaded", function () {
-  const waxSeal = document.querySelector(".wax-seal");
+  const waxSeal = document.querySelector(".wax-seal-clickable");
 
   if (waxSeal) {
     waxSeal.addEventListener("click", function () {
